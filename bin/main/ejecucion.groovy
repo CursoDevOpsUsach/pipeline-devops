f8988c6ec09bc7d0984ec5ef4a245004a1120b4d
duribef <diego.uribe.f@gmail.com>
2022-01-29 10:28:36 -0300

    modificacion LibreriasCompartidas

---
+++ bin/main/ejecucion.groovy
@@ -0,0 +1,54 @@
+/*
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
+*/
+
+def call(){
+  
+
+pipeline {
+    agent any
+    environment {
+        NEXUS_USER         = credentials('user-nexus')
+        NEXUS_PASSWORD     = credentials('password-nexus')
+    }
+    parameters {
+        choice(
+            name:'compileTool',
+            choices: ['Maven', 'Gradle'],
+            description: 'Seleccione herramienta de compilacion'
+        )
+    }
+    stages {
+        stage("Pipeline"){
+            steps {
+                script{
+                  switch(params.compileTool)
+                    {
+                        case 'Maven':
+                            def ejecucion = load 'maven.groovy'
+                            ejecucion.call()
+                        break;
+                        case 'Gradle':
+                            def ejecucion = load 'gradle.groovy'
+                            ejecucion.call()
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    post{
+        success {
+            slackSend color: 'good', message: "[duribef] [${JOB_NAME}] [$compileTool] Ejecucion Exitosa", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-grupo5'
+        }
+        failure {
+            slackSend color: 'danger', message: "[duribef] [${JOB_NAME}] [$compileTool] Ejecucion fallida en stage [${BUILD_ID}]", teamDomain: 'dipdevopsusac-tr94431', tokenCredentialId: 'slack-grupo5'
+        }
+        }
+}
+
+}
+
+return this;
